d75b9f2773016adc326946749f160a2391029735
Nexus0831 <pride0831@gmail.com>
2021-12-06 16:37:10 +0900

    fix: HeaderNavのz-indexを99999から9999に変更

--- src/components/HeaderNav.vue
+++ src/components/HeaderNav.vue
@@ -54,7 +54,7 @@ export default class HeaderNav extends Vue {}
     /* --- end --- */
     /* --- style --- */
     position fixed
-    z-index 99999
+    z-index 9999
     width 100%
     height 50px
     background-color #303030
